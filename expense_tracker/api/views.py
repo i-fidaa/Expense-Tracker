b91f6e2afa30818709b6227af4d1e878d63daf0f
Fidaa Ijmouane <fidaa.ijmouane13@gmail.com>
2025-10-03 23:20:25 +0100

    Add CRUD ViewSets for Category and Expense

--- expense_tracker/api/views.py
+++ expense_tracker/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Category, Expense
+from .serializers import CategorySerializer, ExpenseSerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class ExpenseViewSet(viewsets.ModelViewSet):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+    permission_classes = [permissions.IsAuthenticated]
