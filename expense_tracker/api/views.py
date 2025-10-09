8268b5573175af29fe066e1036a5b414e11ca650
Fidaa Ijmouane <fidaa.ijmouane13@gmail.com>
2025-10-09 21:42:58 +0100

    Add filtering and summary endpoint for expenses

--- expense_tracker/api/views.py
+++ expense_tracker/api/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Category, Expense
 from .serializers import CategorySerializer, ExpenseSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Sum
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
@@ -12,3 +15,9 @@ class ExpenseViewSet(viewsets.ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filterset_fields = ['category', 'date']
+
+    @action(detail=False)
+    def summary(self, request):
+        data = self.queryset.values('category__name').annotate(total=Sum('amount'))
+        return Response(data)
